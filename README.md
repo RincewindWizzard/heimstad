cf10dd3edba8f555f1b6ecae7997f3697df841c6
RincewindWizzard <git@magierdinge.de>
2024-06-08 13:18:23 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# heimstad
+
+Heimstad is (will be) a simple web UI for my smart home offering a
+[rocket](https://rocket.rs/) powered website for a [MQTT](https://de.wikipedia.org/wiki/MQTT)
+backend and some time series database for logging. (Might be PostgreSQL)
+
+Features will include what I need and what I like.
+
+# Installation
+
+Installation should be easy
+
+apt update && apt upgrade -y && apt install -y curl wget gpg
+wget -O- https://rincewindwizzard.github.io/heimstad/deb/KEY.gpg | gpg --dearmor > /etc/apt/trusted.gpg.d/heimstad.gpg
+echo "deb [arch=amd64,signed-by=/etc/apt/trusted.gpg.d/heimstad.gpg]  https://rincewindwizzard.github.io/madriguera-web/deb/ ./" > /etc/apt/sources.list.d/heimstad.list
+apt update
+apt install heimstad
---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+use log::{debug, info, Level};
+
+/// A web UI for my smart home
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Log level
+    #[command(flatten)]
+    verbose: clap_verbosity_flag::Verbosity,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    stderrlog::new()
+        .module(module_path!())
+        .quiet(args.verbose.is_silent())
+        .verbosity(args.verbose.log_level().unwrap_or(Level::Info)) // show warnings and above
+        .timestamp(stderrlog::Timestamp::Millisecond)
+        .init()
+        .expect("Could not setup logging!");
+
+    debug!("Args: {:?}", args);
+    let version = env!("CARGO_PKG_VERSION");
+    println!("You are running heimstad {version}");
+}
