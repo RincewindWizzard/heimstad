372e260b5b42f5bbd936db93d7cfdab28ed728b8
RincewindWizzard <git@magierdinge.de>
2024-06-09 19:22:00 +0200

    eher ein rätsel

--- src/actix_sample.rs
+++ src/actix_sample.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::time::Duration;
 use actix::prelude::*;
 
 
@@ -8,6 +10,10 @@ struct Measurement(i64);
 #[derive(Message)]
 #[rtype(result = "()")]
 struct ResultSum(i64);
+
+struct Emitter {
+    dst: Addr<SumActor>,
+}
 
 struct SumActor {
     sum: i64,
@@ -25,11 +31,21 @@ impl Actor for SumActor {
     type Context = Context<Self>;
 }
 
+impl Actor for Emitter {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Self::Context) {
+        ctx.run_interval(Duration::from_millis(100), |act, ctx| {
+            act.dst.do_send(Measurement(1));
+        });
+    }
+}
+
 impl Handler<ResultSum> for SumPrinter {
     type Result = ();
 
-    fn handle(&mut self, msg: ResultSum, ctx: &mut Self::Context) -> Self::Result {
-        println!("Result: {}", msg.0);
+    fn handle(&mut self, msg: ResultSum, _ctx: &mut Self::Context) -> Self::Result {
+        println!("Got: {}", msg.0);
     }
 }
 
@@ -47,19 +63,46 @@ impl Handler<Measurement> for SumActor {
     }
 }
 
+
+#[derive(Message)]
+#[rtype(result = "Result<String, ()>")]
+enum MyMessage {
+    Ping,
+    Pong,
+    Shutdown,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+struct Message<T> {
+    topic: Topic,
+    payload: T,
+}
+
+struct Topic(String);
+
+pub trait Subscriber<T>: Actor<Context=Context<Self>> + Handler<Message<T>> + Send {}
+
+impl<T, A> Subscriber<T> for A where A: Actor<Context=Context<A>> + Handler<Message<T>> + Send {}
+
+
+struct MessageBroker {
+    subscribers: HashMap<Topic, Vec<Addr<Box<dyn Subscriber<_, Context=(), Result=()>>>>>,
+}
+
 #[cfg(test)]
 mod tests {
+    use std::time::Duration;
     use actix::{Actor, Arbiter, System};
-    use crate::actix_sample::{Measurement, SumActor, SumPrinter};
+    use crate::actix_sample::{Emitter, Measurement, SumActor, SumPrinter};
 
     #[actix_rt::test]
     async fn test_actix() -> Result<(), anyhow::Error> {
-        let printActor = SumPrinter {}.start();
-        let addr = SumActor { sum: 0, dst: printActor }.start();
+        let printer = SumPrinter {}.start();
+        let sum_actor = SumActor { sum: 0, dst: printer }.start();
+        let emitter = Emitter { dst: sum_actor }.start();
 
-        for i in 0..10 {
-            let _ = addr.send(Measurement(i)).await?;
-        }
+        tokio::time::sleep(Duration::from_secs(3)).await;
 
         Ok(())
     }
